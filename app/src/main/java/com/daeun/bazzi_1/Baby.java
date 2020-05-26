faa3dc8abdd201ce54c584aa9a1a7cf401440ea7
Dauuni <ttaun0204@naver.com>
2020-05-26 16:55:49 +0900

    20.05.26 / 16:55

--- app/src/main/java/com/daeun/bazzi_1/Baby.java
+++ app/src/main/java/com/daeun/bazzi_1/Baby.java
@@ -1,5 +1,7 @@
 package com.daeun.bazzi_1;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -7,11 +9,38 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Baby extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener{
+
+    private static String TAG = "mysql";
+
+    private static final String TAG_JSON="sensor";
+    private static final String TAG_TEMP = "temp";
+    private static final String TAG_BPM ="bpm";
+
+    ArrayList<HashMap<String, String>> mArrayList;
+    ListView mlistView;
+    String mJsonString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +48,12 @@ public class Baby extends AppCompatActivity
         setContentView(R.layout.baby);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mlistView = (ListView) findViewById(R.id.listView_main_list3);
+        mArrayList = new ArrayList<>();
+
+        Baby.GetData task = new Baby.GetData();
+        task.execute("http://172.20.10.3/phpinfo.php");
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.Baby);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -29,6 +64,129 @@ public class Baby extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setItemIconTintList(null);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private class GetData extends AsyncTask<String, Void, String>{
+        ProgressDialog progressDialog;
+        String errorString = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(Baby.this,
+                    "Please Wait", null, true, true);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            progressDialog.dismiss();
+            Log.d(TAG, "response  - " + result);
+
+            if (result == null){
+
+            }
+            else {
+
+                mJsonString = result;
+                showResult();
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String serverURL = params[0];
+
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.connect();
+
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+
+                bufferedReader.close();
+
+
+                return sb.toString().trim();
+
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "InsertData: Error ", e);
+                errorString = e.toString();
+
+                return null;
+            }
+
+        }
+    }
+
+    private void showResult(){
+        try {
+            JSONObject jsonObject = new JSONObject(mJsonString);
+            JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
+
+            int i=jsonArray.length()-1;
+
+            JSONObject item = jsonArray.getJSONObject(i);
+
+            String id = item.getString(TAG_TEMP);
+            String password = item.getString(TAG_BPM);
+            //String name = item.getString(TAG_NAME);
+
+            HashMap<String,String> hashMap = new HashMap<>();
+
+            hashMap.put(TAG_TEMP, id);
+            hashMap.put(TAG_BPM, password);
+            //hashMap.put(TAG_NAME, name);
+
+            mArrayList.add(hashMap);
+
+
+            ListAdapter adapter = new SimpleAdapter(
+                    Baby.this, mArrayList, R.layout.item_list3,
+                    new String[]{TAG_TEMP,TAG_BPM},
+                    new int[]{R.id.baby_heat, R.id.baby_pulse}
+            );
+
+            mlistView.setAdapter(adapter);
+
+        } catch (JSONException e) {
+
+            Log.d(TAG, "showResult : ", e);
+        }
+
     }
 
     @Override
