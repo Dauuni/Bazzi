1ed7f8b56f49c27ec9302c2ed5d104f8bc220023
Dauuni <ttaun0204@naver.com>
2020-06-03 11:45:40 +0900

    20.06.03 / 11:45

--- app/src/main/java/com/daeun/bazzi_1/Graph.java
+++ app/src/main/java/com/daeun/bazzi_1/Graph.java
@@ -1,5 +1,6 @@
 package com.daeun.bazzi_1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -9,8 +10,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Graph extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener{
@@ -28,6 +38,41 @@ public class Graph extends AppCompatActivity
         cm=findViewById(R.id.cm);
         kg=findViewById(R.id.kg);
         graph_register=findViewById(R.id.graph_register);
+
+        graph_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //EditText에 입력된 현재 입력되어 있는 값을 get 해온다.
+                String babyCm = cm.getText().toString();
+                String babyKg = kg.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+                            if (success) {
+                                Toast.makeText(getApplicationContext(),"저장에 성공하였습니다.",Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(Graph.this, Graph.class);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(getApplicationContext(),"저장에 실패하였습니다.",Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+
+                //서버로 volley를 이용해서 요청을 함.
+                GraphRequest graphRequest = new GraphRequest(babyCm, babyKg,responseListener);
+                RequestQueue queue = Volley.newRequestQueue(Graph.this);
+                queue.add(graphRequest);
+            }
+        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.Graph);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
