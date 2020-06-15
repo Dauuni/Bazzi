305b9184854dec6fe97d244ad594a9cdc3581690
Dauuni <ttaun0204@naver.com>
2020-06-15 20:03:09 +0900

    20.06.15 / 20:03

--- app/src/main/java/com/daeun/bazzi_1/MainActivity.java
+++ app/src/main/java/com/daeun/bazzi_1/MainActivity.java
@@ -2,6 +2,7 @@ package com.daeun.bazzi_1;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
@@ -15,9 +16,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.util.Log;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -41,6 +44,9 @@ public class MainActivity extends AppCompatActivity
     private static final String TAG_MW = "babyMW";
     private static final String TAG_AGE ="babyAge";
 
+    private final int GET_GALLERY_IMAGE = 200;
+    private ImageView imageview;
+
     ArrayList<HashMap<String, String>> mArrayList;
     ListView mlistView;
     String mJsonString;
@@ -60,11 +66,21 @@ public class MainActivity extends AppCompatActivity
         ImageButton btnGraph = (ImageButton) findViewById(R.id.imageButton5);
         ImageButton btnDiary = (ImageButton) findViewById(R.id.imageButton6);
 
+        imageview = (ImageView)findViewById(R.id.imageView);
+
         mlistView = (ListView) findViewById(R.id.listView_main_list);
         mArrayList = new ArrayList<>();
 
         GetData task = new GetData();
         task.execute("http://bazzi.dothome.co.kr/BabyGetjson.php");
+
+        imageview.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent, GET_GALLERY_IMAGE);
+            }
+        });
 
         btnInform.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -123,6 +139,16 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setItemIconTintList(null);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri selectedImageUri = data.getData();
+            imageview.setImageURI(selectedImageUri);
+        }
     }
 
     private class GetData extends AsyncTask<String, Void, String>{
